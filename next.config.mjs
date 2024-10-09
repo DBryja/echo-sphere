995342f3382a098289d8d32f1185ce7cec18ba77
DBryja <dawbryja@gmail.com>
2024-10-09 15:27:26 +0200

    Config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 import { withPayload } from "@payloadcms/next/withPayload";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    typescript:{
+        ignoreBuildErrors: true
+    }
+};
 
 export default withPayload(nextConfig);
